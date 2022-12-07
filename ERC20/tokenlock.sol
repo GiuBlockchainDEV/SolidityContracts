300a33822310e920b9c59b17a1b909261d2caa6d
Giuliano Neroni <95681714+GiuNeroniDEV@users.noreply.github.com>
2022-12-07 13:45:51 +0100

    Update tokenlock.sol

--- ERC20/tokenlock.sol
+++ ERC20/tokenlock.sol
@@ -1,8 +1,18 @@
 pragma solidity ^0.6.0;
 
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);}
+
 contract TokenLock {
     // Riferimento al contratto del token
-    Token public token;
+    IERC20 public token;
 
     // Mappatura degli equilibri bloccati degli indirizzi
     mapping (address => uint256) public lockedBalanceOf;
@@ -13,23 +23,23 @@ contract TokenLock {
     // Costruttore del contratto
     constructor(address _tokenAddress) public {
         // Imposta il riferimento al contratto del token
-        token = Token(_tokenAddress);
+        token = IERC20(_tokenAddress);
     }
 
     // Funzione di blocco dei token
     function lock(uint256 _value, uint256 _duration) public {
         // Controlla che il mittente abbia un equilibrio sufficiente
-        require(token.balanceOf[msg.sender] >= _value, "Saldo insufficiente");
+        require(token.balanceOf(msg.sender) >= _value, "Saldo insufficiente");
 
         // Calcola la data di sblocco
-        uint256 unlockAt = now + _duration;
+        uint256 unlockAtTime = now + _duration;
 
         // Trasferisci i token dall'indirizzo del mittente all'indirizzo del contratto
         token.transfer(address(this), _value);
 
         // Aggiorna gli equilibri e i tempi di sblocco
         lockedBalanceOf[msg.sender] += _value;
-        unlockAt[msg.sender] = unlockAt;
+        unlockAt[msg.sender] = unlockAtTime;
     }
 
     // Funzione di sblocco dei token
