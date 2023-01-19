4735c1ca06fe102016eacf0402a5b9b1a531cc0a
Giuliano Neroni <95681714+GiuNeroniDEV@users.noreply.github.com>
2023-01-19 10:27:32 +0100

    Create interface.sol

---
+++ ERC721/DEGEN/NFT-PFP/interface.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+interface new_type_IERC20 {
+    function transfer(address, uint) external returns (bool);}
+
+interface old_type_IERC20 {
+    function transfer(address, uint) external;}
+
+interface IERC721A {
+    error ApprovalCallerNotOwnerNorApproved();
+    error ApprovalQueryForNonexistentToken();
+    error ApproveToCaller();
+    error ApprovalToCurrentOwner();
+    error BalanceQueryForZeroAddress();
+    error MintToZeroAddress();
+    error MintZeroQuantity();
+    error OwnerQueryForNonexistentToken();
+    error TransferCallerNotOwnerNorApproved();
+    error TransferFromIncorrectOwner();
+    error TransferToNonERC721ReceiverImplementer();
+    error TransferToZeroAddress();
+    error URIQueryForNonexistentToken();
+
+    struct TokenOwnership {
+        address addr;
+        uint64 startTimestamp;
+        bool burned;}
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    function totalSupply() external view returns (uint256);
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+    function balanceOf(address owner) external view returns (uint256 balance);
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+    function approve(address to, uint256 tokenId) external;
+    function setApprovalForAll(address operator, bool _approved) external;
+    function getApproved(uint256 tokenId) external view returns (address operator);
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+    function transferFrom(address from, address to, uint256 tokenId) external;}
+
+interface ERC721A__IERC721Receiver {
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) external returns (bytes4);}
+
+interface IERC4907 {
+
+    // Logged when the user of an NFT is changed or expires is changed
+    /// @notice Emitted when the `user` of an NFT or the `expires` of the `user` is changed
+    /// The zero address for user indicates that there is no user address
+    event UpdateUser(uint256 indexed tokenId, address indexed user, uint64 expires);
+
+    /// @notice set the user and expires of an NFT
+    /// @dev The zero address indicates there is no user
+    /// Throws if `tokenId` is not valid NFT
+    /// @param user  The new user of the NFT
+    /// @param expires  UNIX timestamp, The new user could use the NFT before expires
+    function setUser(uint256 tokenId, address user, uint64 expires) external;
+
+    /// @notice Get the user address of an NFT
+    /// @dev The zero address indicates that there is no user or the user is expired
+    /// @param tokenId The NFT to get the user address for
+    /// @return The user address for this NFT
+    function userOf(uint256 tokenId) external view returns(address);
+
+    /// @notice Get the user expires of an NFT
+    /// @dev The zero value indicates that there is no user
+    /// @param tokenId The NFT to get the user expires for
+    /// @return The user expires for this NFT
+    function userExpires(uint256 tokenId) external view returns(uint256);}
