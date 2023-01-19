0c28efed1f0edb0db4f1db198b599722409e4f54
Giuliano Neroni <95681714+GiuNeroniDEV@users.noreply.github.com>
2023-01-19 10:26:04 +0100

    Update pfp.sol

--- ERC721/DEGEN/NFT-PFP/pfp.sol
+++ ERC721/DEGEN/NFT-PFP/pfp.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.15;
 
 //  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _                         
 // |   ____  _          ____                 |
@@ -18,38 +18,46 @@ import "./library.sol";
 //Giuliano Neroni DEV
 //https://www.giulianoneroni.com/
 
-contract degenNFT is ERC721A, Ownable, ReentrancyGuard {
+contract degenNFT is ERC721A, Ownable, ReentrancyGuard, IERC4907 {
 
-    struct company_link{
-        string id_company; //STATE + VAT number (VT) or company number (CN) -> IT/VT/11292211007 or UK/CN/13424753
-        uint256 timestamp;}
+    struct UserInfo{
+        // address of user role
+        address user;   
+        // unix timestamp, user expires
+        uint64 expires;}
 
-    mapping(uint => mapping(uint => company_link)) public link;
-    mapping(uint => uint) public linked_company;
+    mapping (uint256  => UserInfo) private _users;
 
     using Strings for uint256;
 
-    string public uriPrefix = "";
+    string public mUriPrefix = "";
+    string public fUriPrefix = "";
+    string public xUriPrefix = "";
     string public uriSuffix = ".json";
 
     uint256 public costWhitelist = 0.01 ether;
     uint256 public costPublicSale = 0.02 ether;
+    uint256 public costGenderChange = 0.02 ether;
     uint256 public NFTminted;
 
     bool public paused = true;
     bool public whitelistMintEnabled = false;
     bool public revealed = false;
 
-    mapping (address => bool) public whitelisted;
+    mapping(address => bool) public whitelisted;
     mapping(address => uint) public minted;
+    mapping(uint => uint) public edited;
+
+    mapping(uint => uint) public sex;
+    mapping(uint => mapping (uint => string)) public uri;
+
 
     string public tokenName = "DEGEN NFT COLLECTION";
     string public tokenSymbol = "DNC";
     uint256 public maxSupply = 10420;
     uint256 public mintableSupply = 10000;
     uint256 public maxMintAmountPerTx = 200;
-    string public hiddenMetadataUri = "ipfs://bafybeibgmbc3cfamhby6z43jr2pnx3s2u7f22qvbp2hiptisilfdccq3z4";
-
+    string public hiddenMetadataUri = "ipfs://QmRp5WBQuC56cVJJ5qnXkprtNT64ofdfa8nADfhVEWPhe9";
     
     constructor() ERC721A(tokenName, tokenSymbol) {
             maxSupply = maxSupply;
@@ -68,30 +76,9 @@ contract degenNFT is ERC721A, Ownable, ReentrancyGuard {
             require(msg.value >= costPublicSale * _mintAmount, "Insufficient funds!");}
         _;}
 
-    function create_link(address _wallet_address, string memory _id_company, uint256 _token_id) public onlyOwner { 
-        require(check_token_owned(_wallet_address, _token_id) == 1, "Token not owned");  
-        uint i = linked_company[_token_id];
-        linked_company[_token_id] += 1;
-        link[_token_id][i].id_company = _id_company;
-        link[_token_id][i].timestamp = block.timestamp;}
-
-    function check_all_token_owned(address _wallet_address) public view returns (uint256[] memory valueReturn){
-        require(totalSupply() > 0, "0 NFT Minted");
-        uint256 mintedNFT = totalSupply();
-        uint256 counter;
-        uint256[] memory value = new uint256[](balanceOf(_wallet_address));
-        for (uint i = 1; i <= mintedNFT; i++) {
-            if(ownerOf(i) == _wallet_address){
-                value[counter] = i;
-                counter ++;}}         
-        return value;}
-
-    function check_token_owned(address _wallet_address, uint256 token_id) public view returns (uint8 valueReturn){
-        require(totalSupply() > 0, "0 NFT Minted");
-        uint8 value;
-        if(ownerOf(token_id) == _wallet_address){
-                value = 1;}         
-        return value;}
+    modifier genderChangePriceCompliance() {
+        require(msg.value >= costGenderChange, "Insufficient funds!");
+        _;}
 
     function setCostWhitelist(uint256 _cost) public onlyOwner {
         costWhitelist = _cost;}
@@ -99,31 +86,74 @@ contract degenNFT is ERC721A, Ownable, ReentrancyGuard {
     function setCostPublicSale(uint256 _cost) public onlyOwner {
         costPublicSale = _cost;}
 
-    function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) mintPriceCompliance(_mintAmount) {
+    function mint(uint256 _mintAmount, uint256 _sex) public payable mintCompliance(_mintAmount) mintPriceCompliance(_mintAmount) {
         require(!paused, 'The contract is paused!');
         minted[_msgSender()] = minted[_msgSender()] + _mintAmount;//CHECK
         require(minted[_msgSender()] <= maxMintAmountPerTx, "Max quantity reached");
+        uint256 nftMinted = NFTminted;
         NFTminted += _mintAmount;
-            _safeMint(_msgSender(), _mintAmount);}
+        _safeMint(_msgSender(), _mintAmount);
+        for (uint i = 1; i <= _mintAmount; i++) {
+            uint256 NFT = nftMinted + i;
+            sex[NFT] = _sex;}}
+
+    function mintForAddress(uint256 _mintAmount, address _receiver, uint256 _sex) public onlyOwner {
+        require(totalSupply() + _mintAmount <= maxSupply, 'Max supply exceeded!');
+        //Minted by Owner without any cost, doesn't count on minted quantity
+        uint256 nftMinted = NFTminted;
+        NFTminted += _mintAmount;
+        _safeMint(_receiver, _mintAmount);
+        for (uint i = 1; i <= _mintAmount; i++) {
+            uint256 NFT = nftMinted + i;
+            sex[NFT] = _sex;}}
+
+    function genderChange(uint256 _sex, uint256 _tokenId) public payable genderChangePriceCompliance() {
+        require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
+        require((ownerOf(_tokenId) == address(_msgSender()) || getApproved(_tokenId) == address(_msgSender())),"ERC721A: changer caller is not owner nor approved");
+        require(_sex != sex[_tokenId], "Same Sex");
+        sex[_tokenId] = _sex;}
+
+    function genderView(uint256 _tokenId) public view returns (string memory _sex) {
+        require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
+        if(sex[_tokenId] == 0){
+            _sex = "X";}
+        if(sex[_tokenId] == 1){
+            _sex = "M";}
+        if(sex[_tokenId] == 2){
+            _sex = "F";}
+        return(_sex);}
 
     function burn(uint256 tokenId) public {
         _burn(tokenId, true); }
 
-    function mintForAddress(uint256 _mintAmount, address _receiver) public onlyOwner {
-        require(totalSupply() + _mintAmount <= maxSupply, 'Max supply exceeded!');
-        //Minted by Owner without any cost, doesn't count on minted quantity
-        NFTminted += _mintAmount;
-        _safeMint(_receiver, _mintAmount);}
-
     function _startTokenId() internal view virtual override returns (uint256) {
         return 1;}
 
+    function setUri(uint _tokenId, string memory xUri, string memory mUri, string memory fUri) public onlyOwner {
+        uri[_tokenId][0] = xUri;
+        uri[_tokenId][1] = mUri;
+        uri[_tokenId][2] = fUri;
+        edited[_tokenId] = 1;}
+
     function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
-        require(_exists(_tokenId), 'ERC721Metadata: URI query for nonexistent token');
+        require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
         if (revealed == false) {
             return hiddenMetadataUri;}
-        string memory currentBaseURI = _baseURI();
-        return bytes(currentBaseURI).length > 0 ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix)): '';}
+        if (revealed == true) {
+            string memory currentBaseURI;
+            if(sex[_tokenId] == 0){
+                currentBaseURI = string(abi.encodePacked(xUriPrefix, _tokenId.toString(), uriSuffix));
+                if(edited[_tokenId] == 1){
+                    currentBaseURI = uri[_tokenId][0];}}
+            if(sex[_tokenId] == 1){
+                currentBaseURI = string(abi.encodePacked(mUriPrefix, _tokenId.toString(), uriSuffix));
+                if(edited[_tokenId] == 1){
+                    currentBaseURI = uri[_tokenId][1];}}
+            if(sex[_tokenId] == 2){
+                currentBaseURI = string(abi.encodePacked(fUriPrefix, _tokenId.toString(), uriSuffix));
+                if(edited[_tokenId] == 1){
+                    currentBaseURI = uri[_tokenId][2];}}
+            return currentBaseURI;}}
     
     function setRevealed(bool _state) public onlyOwner {
         revealed = _state;}
@@ -134,8 +164,10 @@ contract degenNFT is ERC721A, Ownable, ReentrancyGuard {
     function setHiddenMetadataUri(string memory _hiddenMetadataUri) public onlyOwner {
         hiddenMetadataUri = _hiddenMetadataUri;}
 
-    function setUriPrefix(string memory _uriPrefix) public onlyOwner {
-        uriPrefix = _uriPrefix;}
+    function setUriPrefix(string memory _mUriPrefix, string memory _fUriPrefix, string memory _xUriPrefix) public onlyOwner {
+        mUriPrefix = _mUriPrefix;
+        fUriPrefix = _fUriPrefix;
+        xUriPrefix = _xUriPrefix;}
 
     function setUriSuffix(string memory _uriSuffix) public onlyOwner {
         uriSuffix = _uriSuffix;}
@@ -167,5 +199,33 @@ contract degenNFT is ERC721A, Ownable, ReentrancyGuard {
         (bool os, ) = payable(owner()).call{value: address(this).balance}('');
         require(os);}
         
-    function _baseURI() internal view virtual override returns (string memory) {
-        return uriPrefix;}}
+    /// @notice set the user and expires of a NFT
+    /// @dev The zero address indicates there is no user 
+    /// Throws if `tokenId` is not valid NFT
+    /// @param user  The new user of the NFT
+    /// @param expires  UNIX timestamp, The new user could use the NFT before expires
+    function setUser(uint256 tokenId, address user, uint64 expires) public override virtual{
+        require((ownerOf(tokenId) == address(_msgSender()) || getApproved(tokenId) == address(_msgSender())),"ERC721A: rental caller is not owner nor approved");
+        require(userOf(tokenId) == address(0),"User already assigned");
+        require(expires > block.timestamp, "Expires should be in future");
+        UserInfo storage info =  _users[tokenId];
+        info.user = user;
+        info.expires = expires;
+        emit UpdateUser(tokenId,user,expires);}
+
+    /// @notice Get the user address of an NFT
+    /// @dev The zero address indicates that there is no user or the user is expired
+    /// @param tokenId The NFT to get the user address for
+    /// @return The user address for this NFT
+    function userOf(uint256 tokenId) public view override virtual returns(address){
+        if( uint256(_users[tokenId].expires) >=  block.timestamp){
+            return _users[tokenId].user; }
+        return address(0);}
+
+    /// @notice Get the user expires of an NFT
+    /// @dev The zero value indicates that there is no user 
+    /// @param tokenId The NFT to get the user expires for
+    /// @return The user expires for this NFT
+    function userExpires(uint256 tokenId) public view override virtual returns(uint256){
+        return _users[tokenId].expires;}}
+    
