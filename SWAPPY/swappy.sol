914908d8ff0957ea037221d9774d4ed2a396bac7
Giuliano Neroni <95681714+GiuNeroniDEV@users.noreply.github.com>
2023-03-07 12:02:36 +0100

    Update swappy.sol

--- SWAPPY/swappy.sol
+++ SWAPPY/swappy.sol
@@ -1,13 +1,15 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
 
-//  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _                         
-// |   ____  _          ____                 |
-// |  / ___|(_) _   _  |  _ \   ___ __   __  |
-// | | |  _ | || | | | | | | | / _ \\ \ / /  |
-// | | |_| || || |_| | | |_| ||  __/ \ V /   |
-// |  \____||_| \__,_| |____/  \___|  \_/    |
-// | _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ | 
+//    .-'''-. .--.      .--.   ____    .-------. .-------.  ____     __  
+//   / _     \|  |_     |  | .'  __ `. \  _(`)_ \\  _(`)_ \ \   \   /  / 
+//  (`' )/`--'| _( )_   |  |/   '  \  \| (_ o._)|| (_ o._)|  \  _. /  '  
+// (_ o _).   |(_ o _)  |  ||___|  /  ||  (_,_) /|  (_,_) /   _( )_ .'   
+//  (_,_). '. | (_,_) \ |  |   _.-`   ||   '-.-' |   '-.-'___(_ o _)'    
+// .---.  \  :|  |/    \|  |.'   _    ||   |     |   |   |   |(_,_)'     
+// \    `-'  ||  '  /\  `  ||  _( )_  ||   |     |   |   |   `-'  /      
+//  \       / |    /  \    |\ (_ o _) //   )     /   )    \      /       
+//   `-...-'  `---'    `---` '.(_,_).' `---'     `---'     `-..-'        
 
 //Giuliano Neroni DEV
 //https://www.giulianoneroni.com/
@@ -18,32 +20,56 @@ import "./interface.sol";
 contract Swappy is Ownable, ReentrancyGuard {
 
     address public swappyWallet;
+    address public limitWallet;
     using SafeERC20 for IERC20;
+
+    //maxApproveValue = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
 
     fallback() external payable {}
     receive() external payable {}
 
+    //Wallet Setting
+
     function setSwappyWallet(address _swappyWallet) public onlyOwner() {
         swappyWallet = _swappyWallet;}
 
-    function tokenBalanceAllowance(address _tokenToSwap) public view returns (uint256, uint256) {
-        IERC20 tokenToSwap = IERC20(_tokenToSwap);
-        uint256 balance = tokenToSwap.balanceOf(msg.sender);
-        uint256 allowance = tokenToSwap.allowance(msg.sender, address(this));
-        return (balance, allowance);}
+    function setLimitWallet(address _limitWallet) public onlyOwner() {
+        limitWallet = _limitWallet;}
 
-    function swappyAnyToken(address _tokenToSwap, uint256 amount, address tokenTo, string memory blockchainTo, string memory differentWallet, uint256 buyOrSell, uint256 expiration, string memory refCode) external {
+    //Operative function
+
+    function swappyAnyToken(address _tokenToSwap, uint256 amount, address tokenTo, string memory blockchainTo, string memory differentWallet, string memory refCode) public payable {
         IERC20 tokenToSwap = IERC20(_tokenToSwap);
         uint256 balance = tokenToSwap.balanceOf(msg.sender);
         require(balance > 0, "You have no token to swap");
         require(tokenToSwap.allowance(msg.sender, address(this)) >= amount, "Approve Necessary");
-        //maxApproveValue = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
         tokenToSwap.safeTransferFrom(msg.sender, swappyWallet, amount);}
 
-    function swappyETH(uint256 amount, address tokenTo, string memory blockchainTo, string memory differentWallet, uint256 buyOrSell, uint256 expiration, string memory refCode) public payable {
-        require(msg.sender.balance > 0, "You have no token to swap");
-        (bool sent,) = swappyWallet.call{value: amount}("");
+    function swappyETH(uint256 amount, address tokenTo, string memory blockchainTo, string memory differentWallet, string memory refCode) public payable {
+        require(msg.sender.balance > (amount), "You have no token to swap");
+        (bool sent,) = swappyWallet.call{value: (amount)}("");
         require(sent, "Failed to send Ether");}
+        
+    function limitBuy(address _tokenToSwap, uint256 amount, address tokenTo, string memory blockchainTo, string memory differentWallet, string memory condition, string memory price, uint256 expiration, string memory refCode) public {
+        IERC20 tokenToSwap = IERC20(_tokenToSwap);
+        uint256 balance = tokenToSwap.balanceOf(msg.sender);
+        require(balance > 0, "You have no token to swap");
+        require(tokenToSwap.allowance(msg.sender, address(this)) >= amount, "Approve Necessary");
+        tokenToSwap.safeTransferFrom(msg.sender, limitWallet, amount);}
+
+    function limitSellAnyToken(address _tokenToSwap, uint256 amount, address tokenTo, string memory blockchainTo, string memory differentWallet, string memory price_gain, string memory price_loss, uint256 expiration, string memory refCode) public {
+        IERC20 tokenToSwap = IERC20(_tokenToSwap);
+        uint256 balance = tokenToSwap.balanceOf(msg.sender);
+        require(balance > 0, "You have no token to swap");
+        require(tokenToSwap.allowance(msg.sender, address(this)) >= amount, "Approve Necessary");
+        tokenToSwap.safeTransferFrom(msg.sender, limitWallet, amount);}    
+
+    function limitSellETH(uint256 amount, address tokenTo, string memory blockchainTo, string memory differentWallet, string memory price_gain, string memory price_loss, uint256 expiration, string memory refCode) public payable {
+        require(msg.sender.balance > 0, "You have no token to swap");
+        (bool sent,) = limitWallet.call{value: amount}("");
+        require(sent, "Failed to send Ether");}  
+
+    //Security function
 
     function transferAnyNewERC20Token(address _tokenAddr, address _to, uint _amount) public onlyOwner {  
         require(NewIERC20(_tokenAddr).transfer(_to, _amount), "Could not transfer out tokens!");}
