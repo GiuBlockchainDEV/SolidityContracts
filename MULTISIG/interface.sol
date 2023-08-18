d3a9fed1c556561b66a3ad563b23d7951ca96088
Giuliano Neroni <95681714+GiuNeroniDEV@users.noreply.github.com>
2023-08-18 21:36:32 +0200

    Update interface.sol

--- MULTISIG/interface.sol
+++ MULTISIG/interface.sol
@@ -19,4 +19,5 @@ interface IERC20Metadata is IERC20 {
 
 interface IERC20Permit {
     function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
+    function nonces(address owner) external view returns (uint256);
     function DOMAIN_SEPARATOR() external view returns (bytes32);}
