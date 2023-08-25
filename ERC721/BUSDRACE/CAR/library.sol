607f7a1f4d73514592353fc8a2e924106a7be72b
Giuliano Neroni <95681714+GiuNeroniDEV@users.noreply.github.com>
2023-08-26 01:35:59 +0200

    Update library.sol

--- ERC721/BUSDRACE/CAR/library.sol
+++ ERC721/BUSDRACE/CAR/library.sol
@@ -47,12 +47,10 @@ library SafeMath {
   */
   function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
     if (a == 0) {
-      return 0;
-    }
+      return 0;}
     c = a * b;
     assert(c / a == b);
-    return c;
-  }
+    return c;}
   /**
   * @dev Integer division of two numbers, truncating the quotient.
   */
@@ -60,21 +58,17 @@ library SafeMath {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
     // uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
+    return a / b;}
   /**
   * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
   */
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
-    return a - b;
-  }
+    return a - b;}
   /**
   * @dev Adds two numbers, throws on overflow.
   */
   function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
     c = a + b;
     assert(c >= a);
-    return c;
-  }
-}
+    return c;}}
