f1e59cdf6de9af2a926993c545a10481d99e4632
Giuliano Neroni <95681714+GiuNeroniDEV@users.noreply.github.com>
2023-08-27 17:08:11 +0200

    Update interface.sol

--- ERC721/BUSDRACE/NOS/interface.sol
+++ ERC721/BUSDRACE/NOS/interface.sol
@@ -193,3 +193,4 @@ interface IERC20 {
 
 interface ERC721A__IERC721Receiver {
     function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) external returns (bytes4);}
+
