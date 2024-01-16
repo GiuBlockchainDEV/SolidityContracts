04ceeef95f971ea4d1067b1e795ba05ae0dfbb04
Giuliano <95681714+GiuBlockchainDEV@users.noreply.github.com>
2024-01-16 16:25:00 +0100

    Create mili.sol

---
+++ ERC20/Mili/mili.sol
@@ -0,0 +1,256 @@
+// SPDX-License-Identifier: MIT
+/*
+
+,---.    ,---..-./`)   .---.    .-./`)  
+|    \  /    |\ .-.')  | ,_|    \ .-.') 
+|  ,  \/  ,  |/ `-' \,-./  )    / `-' \ 
+|  |\_   /|  | `-'`"`\  '_ '`)   `-'`"` 
+|  _( )_/ |  | .---.  > (_)  )   .---.  
+| (_ o _) |  | |   | (  .  .-'   |   |  
+|  (_,_)  |  | |   |  `-'`-'|___ |   |  
+|  |      |  | |   |   |        \|   |  
+'--'      '--' '---'   `--------`'---'  
+                                        
+*/
+
+pragma solidity ^0.8.20;
+
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+abstract contract Ownable is Context {
+    address private _owner;
+    error OwnableUnauthorizedAccount(address account);
+    error OwnableInvalidOwner(address owner);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    constructor(address initialOwner) {
+        if (initialOwner == address(0)) {
+            revert OwnableInvalidOwner(address(0));
+        }
+        _transferOwnership(initialOwner);
+    }
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+    function _checkOwner() internal view virtual {
+        if (owner() != _msgSender()) {
+            revert OwnableUnauthorizedAccount(_msgSender());
+        }
+    }
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        if (newOwner == address(0)) {
+            revert OwnableInvalidOwner(address(0));
+        }
+        _transferOwnership(newOwner);
+    }
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+interface IERC20Errors {
+    error ERC20InsufficientBalance(address sender, uint256 balance, uint256 needed);
+    error ERC20InvalidSender(address sender);
+    error ERC20InvalidReceiver(address receiver);
+    error ERC20InsufficientAllowance(address spender, uint256 allowance, uint256 needed);
+    error ERC20InvalidApprover(address approver);
+    error ERC20InvalidSpender(address spender);
+}
+
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+interface IERC20Metadata is IERC20 {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+}
+
+abstract contract ERC20 is Context, IERC20, IERC20Metadata, IERC20Errors {
+    mapping(address account => uint256) private _balances;
+    mapping(address account => mapping(address spender => uint256)) private _allowances;
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+    function name() public view virtual returns (string memory) {
+        return _name;
+    }
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
+    }
+    function decimals() public view virtual returns (uint8) {
+        return 18;
+    }
+    function totalSupply() public view virtual returns (uint256) {
+        return _totalSupply;
+    }
+    function balanceOf(address account) public view virtual returns (uint256) {
+        return _balances[account];
+    }
+    function transfer(address to, uint256 value) public virtual returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, value);
+        return true;
+    }
+    function allowance(address owner, address spender) public view virtual returns (uint256) {
+        return _allowances[owner][spender];
+    }
+    function approve(address spender, uint256 value) public virtual returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, value);
+        return true;
+    }
+    function transferFrom(address from, address to, uint256 value) public virtual returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, value);
+        _transfer(from, to, value);
+        return true;
+    }
+    function _transfer(address from, address to, uint256 value) internal {
+        if (from == address(0)) {
+            revert ERC20InvalidSender(address(0));
+        }
+        if (to == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        _update(from, to, value);
+    }
+    function _update(address from, address to, uint256 value) internal virtual {
+        if (from == address(0)) {
+            _totalSupply += value;
+        } else {
+            uint256 fromBalance = _balances[from];
+            if (fromBalance < value) {
+                revert ERC20InsufficientBalance(from, fromBalance, value);
+            }
+            unchecked {
+                _balances[from] = fromBalance - value;
+            }
+        }
+
+        if (to == address(0)) {
+            unchecked {
+                _totalSupply -= value;
+            }
+        } else {
+            unchecked {
+                _balances[to] += value;
+            }
+        }
+        emit Transfer(from, to, value);
+    }
+    function _mint(address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        _update(address(0), account, value);
+    }
+    function _burn(address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert ERC20InvalidSender(address(0));
+        }
+        _update(account, address(0), value);
+    }
+    function _approve(address owner, address spender, uint256 value) internal {
+        _approve(owner, spender, value, true);
+    }
+    function _approve(address owner, address spender, uint256 value, bool emitEvent) internal virtual {
+        if (owner == address(0)) {
+            revert ERC20InvalidApprover(address(0));
+        }
+        if (spender == address(0)) {
+            revert ERC20InvalidSpender(address(0));
+        }
+        _allowances[owner][spender] = value;
+        if (emitEvent) {
+            emit Approval(owner, spender, value);
+        }
+    }
+    function _spendAllowance(address owner, address spender, uint256 value) internal virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance != type(uint256).max) {
+            if (currentAllowance < value) {
+                revert ERC20InsufficientAllowance(spender, currentAllowance, value);
+            }
+            unchecked {
+                _approve(owner, spender, currentAllowance - value, false);
+            }
+        }
+    }
+}
+
+contract Mili is ERC20, Ownable(msg.sender) {
+    // Lubrication prevents any wallet receiving more than 1% of Token in the opening days.
+    bool public lubricating = true;
+    address public liquidityPool;
+
+    // Mint the totalSupply (69.420 T) to the deployer
+    constructor() ERC20("Mili", "MILI") {
+        _mint(msg.sender, 1000000000 * (10 ** 18));
+    }
+
+    // Function to set lubricating state
+    function setLubricating(bool _state) external onlyOwner {
+        lubricating = _state;
+    }
+
+    // Define the LP address to enable trading!
+    function setLiquidityPool(address _liquidityPool) external onlyOwner {
+        liquidityPool = _liquidityPool;
+    }
+
+    // Override _update function to include lubricating logic (previously _beforeTokenTransfer)
+    function _update(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        super._update(from, to, amount);
+        // If liquidityPool is address(0) we've not yet enabled trading. Liquidity Loading....
+        if(liquidityPool == address(0)) {
+            require(from == owner() || to == owner(), "Patience - Trading Not Started Yet!");
+            return;
+        }
+        // Allow deployer (owner) to send/receive any amount and the liquidityPool to receive any amount.
+        // This allows for loading of the LP, and for people to sell tokens into the LP whilst lubrication in progress.
+        if (lubricating && from != owner() && to != liquidityPool) {
+            // Require that a receiving wallet will not hold more than 1% of supply after a transfer whilst lubrication is in effect
+            require(
+                balanceOf(to) <= totalSupply() / 100,
+                "Just getting warmed up, limit of 1% of Mili can be move until Lubrication is complete!"
+            );
+        }
+    }
+    // Renounce the contract and pass ownership to address(0) to lock the contract forever more.
+    function renounceTokenOwnership() public onlyOwner {
+        renounceOwnership();
+    }
+}
